af50055c99e47e1fdceeb38f79e8721855851657
RenishBhuva07 <renish.bhuva.07@gmail.com>
2022-09-15 01:34:25 +0530

    New Data

---
+++ Tops Task Mode/PSD TO WEB/E-commerce website/E-commerce website/js/main.js
@@ -0,0 +1,21 @@
+
+// nav active 
+
+let nav = document.querySelector(".navigation-wrap");
+ window.onscroll = function(){
+     if(document.documentElement.scrollTop > 20){
+         nav.classList.add("scroll-on")
+     }else{
+         nav.classList.remove("scroll-on")
+     }
+ }
+
+ // nav hidden
+
+let navbar = document.querySelectorAll(".nav-link")
+let navcollapse = document.querySelector(".navbar-collapse.collapse");
+navbar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navcollapse.classList.remove("show");
+    })
+})
